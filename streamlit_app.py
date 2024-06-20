198c6f93d57b22025b20226cd7c501e10bc14feb
acn-davidv <d.van.de.vondervoort@accenture.com>
2024-06-20 15:02:22 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,6 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
 if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
@@ -32,6 +28,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
