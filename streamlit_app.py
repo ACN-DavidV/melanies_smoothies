fb4b7e142fdf846f040baec65315359870aea872
acn-davidv <d.van.de.vondervoort@accenture.com>
2024-06-20 14:58:03 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 st.title(":cup_with_straw: Customise Your Smoothie :cup_with_straw:")
 st.write(
@@ -38,3 +39,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_smootie+'!', icon="✅")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
